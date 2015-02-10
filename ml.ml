65eaa67b439c28e34ac5e414616f061f7a59b5dc
Allen Wu <allen.wu@ucla.edu>
2015-02-09 22:31:41 -0800

    more green

--- ml.ml
+++ ml.ml
@@ -10,3 +10,9 @@ let rec evalAExp exp =
       | Minus ->  (evalAExp left) -. (evalAExp right)
       | Times -> (evalAExp left) *. (evalAExp right)
       | Divide -> (evalAExp left) /. (evalAExp right)
+
+let rec dec2bin (n: int) : int list =
+    match n with
+        0 -> [0]
+        | 1 -> [1]
+        | _ -> (dec2bin (n / 2))@[n mod 2] ;;
