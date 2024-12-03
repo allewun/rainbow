5b53fc80fcdde40ef893bdf332837876da92d572
Allen Wu <allewun@gmail.com>
2024-12-02 23:08:14 -0800

    php shouldn't be interpreted as hack (was gray instead of purple)

--- php.php
+++ php.php
@@ -1,13 +1,13 @@
+<?php
 function getLedColor($status) {
   switch (strtolower($status)) {
-    case 'closed':
-    case 'cancelled': return 'red';
+    case 'closed':    return 'red';
     case 'open':      return 'green';
     case 'w-list':    return 'yellow';
     default:          return '';
   }
 }
-
 function formatPhone($str) {
   return preg_replace('/^(\d{3})(\d{3})(\d{4})(.*)$/', '($1) $2-$3', $str);
 }
+?>
